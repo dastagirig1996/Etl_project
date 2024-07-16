9842ce7ce78d22f44d08a7a1737f9bf9a6cdac5c
dastagirig1996 <111571182+dastagirig1996@users.noreply.github.com>
2024-07-16 18:25:04 +0530

    u;sfsjdfdfd

--- dags/etl_pipeline.py
+++ dags/etl_pipeline.py
@@ -1,5 +1,5 @@
 from airflow import DAG
-from airflow.operators.python_operator import PythonOperator
+from airflow.operators.python import PythonOperator
 from airflow.utils.dates import days_ago
 import sys
 import os
