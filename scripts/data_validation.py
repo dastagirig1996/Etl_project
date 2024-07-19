5f4d18c3242f22ce70a2146c647dfbf0ccea203e
dastagirig1996 <111571182+dastagirig1996@users.noreply.github.com>
2024-07-19 15:30:56 +0530

    cifsjfld

--- scripts/data_validation.py
+++ scripts/data_validation.py
@@ -10,3 +10,8 @@ def load_data():
     from config.aws_credentials import Access_key_ID, Secret_access_key
     s3 = boto3.client("s3",aws_access_key_id = Access_key_ID, aws_secret_access_key = Secret_access_key )
     s3.upload_file("C:\PROFFESSION\Data_Engneer_Projects\Finance_EtL_Project\data\customers_by_year.csv","aja-bucket", "customers_data_by_year.csv")
+# import os
+
+# print(os.getenv("hello"))
+# print(os.getenv("adeed "))
+# # os.environ["adeed"]="123654"
